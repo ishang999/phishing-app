f2d1416c2980b1ef945db984ac6d0e06028dd7d9
ishang999 <girgaonkarik99@gmail.com>
2024-11-24 00:12:21 +0530

    Update sl.py

--- sl.py
+++ sl.py
@@ -1,13 +1,42 @@
-import streamlit as st 
+import streamlit as st
+import pickle
+import numpy as np
+#model=pickle.load(open('model.pkl','rb'))
 
-# Set the app title 
-st.title('My First Streamlit App') 
 
-# Add a welcome message 
-st.write('Welcome to my Streamlit app!') 
 
-# Create a text input widget
-user_input = st.text_input('Enter a custom message:', 'Hello, Streamlit!')
 
-# Display the customized message 
-st.write('Customized Message:', user_input)
+def main():
+    st.title("Streamlit Tutorial")
+    html_temp = """
+    <div style="background-color:#025246 ;padding:10px">
+    <h2 style="color:white;text-align:center;">Forest Fire Prediction ML App </h2>
+    </div>
+    """
+    st.markdown(html_temp, unsafe_allow_html=True)
+
+    oxygen = st.text_input("input1","Type Here")
+    humidity = st.text_input("input2","Type Here")
+    temperature = st.text_input("input3","Type Here")
+    safe_html="""  
+      <div style="background-color:#F4D03F;padding:10px >
+       <h2 style="color:white;text-align:center;"> Your forest is safe</h2>
+       </div>
+    """
+    danger_html="""  
+      <div style="background-color:#F08080;padding:10px >
+       <h2 style="color:black ;text-align:center;"> Your forest is in danger</h2>
+       </div>
+    """
+
+    if st.button("Predict"):
+        output=predict_forest(oxygen,humidity,temperature)
+        st.success('The probability of fire taking place is {}'.format(output))
+
+        if output > 0.5:
+            st.markdown(danger_html,unsafe_allow_html=True)
+        else:
+            st.markdown(safe_html,unsafe_allow_html=True)
+
+if __name__=='__main__':
+    main()
